85d285e0e8f0c19d46125194650f4265e1f464fb
itzhimangshu <itzhimangshu@gmail.com>
2024-04-30 23:50:37 +0530

    initializing package.json and installing dependencies

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', async (req, res) => {
+    res.send('Hello from PHOTO-AI!');
+});
+
+const startServer = async () => {
+    app.listen(8000, () => console.log('Server has started on port http://localhost:8000'))
+}
+
+startServer();
