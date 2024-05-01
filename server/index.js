2b9c6f1c7df53315166b364d9a35225f79141922
itzhimangshu <itzhimangshu@gmail.com>
2024-05-01 19:42:47 +0530

    creating API endpoints

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/post.route.js';
+import dalleRoutes from './routes/dalle.route.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello from PHOTO-AI!');
